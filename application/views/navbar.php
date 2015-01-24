e174b8abf31780280b47e94c0a9f628a33459ea9
Ashley Baldwin <no8fear8@gmail.com>
2015-01-24 22:09:19 +0000

    yet anohter update

---
+++ application/views/navbar.php
@@ -0,0 +1,10 @@
+<div class="row">
+	<div class="span12">
+		<ul class="nav nav-tabs">
+			<li><a href='#'>Home</a></li>
+			<li><a href='#'>Ask Questions</a></li>
+			<li><a href='#'>Answer Questions</a></li>
+			<li><a href='#'>Feedback</a></li>
+		</ul>
+	</div>
+</div>
